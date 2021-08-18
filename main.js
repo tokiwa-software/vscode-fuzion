e5a9c6b81c142c02567d3cd73354a328e6eeb704
michael.lill <michael.lill@posteo.de>
2021-08-18 12:58:45 +0200

    test for presence of java

--- main.js
+++ main.js
@@ -1,61 +1,77 @@
 const net = require('net');
 const vscode = require('vscode');
 const child_process = require('child_process');
-const { LanguageClient } = require('vscode-languageclient/node');
+const { LanguageClient, TransportKind } = require('vscode-languageclient/node');
 
 let client, channel;
 
 function activate(context)
 {
+  const transportKind = TransportKind.socket;
   const debug = context.extensionMode == vscode.ExtensionMode.Development;
   channel = vscode.window.createOutputChannel("vscode-fuzion");
   context.subscriptions.push(channel);
 
-  lspServer = debug
-    ? {
-      command: 'make',
-      arguments: [`debug`, `-C`, `${context.extensionPath}/fuzion-lsp-server/`, `-f`, `${context.extensionPath}/fuzion-lsp-server/Makefile`]
-    }
-    : 'not implemented';
-
-  child_process.spawn(lspServer.command, lspServer.arguments).stdout.on('data', function (data)
+  try
   {
-    const stdOut = data.toString().split('\n');
-    stdOut.forEach(line => channel.appendLine('SERVER: ' + line));
-    const port = stdOut
-      .filter(line => line.startsWith('socket opened on port:'))
-      .map(line => parseInt(line.replace(/\D*/g, '')))
-      .find(() => true);
-    if (!port)
+    return child_process.execSync('java').toString();
+  }
+  catch (error)
+  {
+    if (error.status !== 1)
     {
+      vscode.window.showErrorMessage('Please install Java and make sure it\'s in PATH.');
       return;
     }
+  }
 
-    const serverOptions = () =>
+  if (transportKind === TransportKind.socket)
+  {
+    lspServer = debug
+      ? {
+        command: 'make',
+        arguments: [`debug`, `-C`, `${context.extensionPath}/fuzion-lsp-server/`, `-f`, `${context.extensionPath}/fuzion-lsp-server/Makefile`]
+      }
+      : 'not implemented';
+
+    child_process.spawn(lspServer.command, lspServer.arguments).stdout.on('data', function (data)
     {
-      const socket = net.connect(port);
-      const result = {
-        writer: socket,
-        reader: socket
+      const stdOut = data.toString().split('\n');
+      stdOut.forEach(line => channel.appendLine('SERVER: ' + line));
+      const port = stdOut
+        .filter(line => line.startsWith('socket opened on port:'))
+        .map(line => parseInt(line.replace(/\D*/g, '')))
+        .find(() => true);
+      if (!port)
+      {
+        return;
+      }
+
+      const serverOptions = () =>
+      {
+        const socket = net.connect(port);
+        const result = {
+          writer: socket,
+          reader: socket
+        };
+        return Promise.resolve(result);
       };
-      return Promise.resolve(result);
-    };
 
-    const clientOptions = {
-      documentSelector: [{ scheme: 'file', language: 'Fuzion' }],
+      const clientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'Fuzion' }],
 
-    };
+      };
 
-    client = new LanguageClient(
-      'Fuzion Language Server',
-      serverOptions,
-      clientOptions
-    );
+      client = new LanguageClient(
+        'Fuzion Language Server',
+        serverOptions,
+        clientOptions
+      );
 
-    channel.appendLine('CLIENT: Fuzion Language Client started');
-    client.start();
-  });
-
+      channel.appendLine('CLIENT: Fuzion Language Client started');
+      client.start();
+    });
+  }
 
 }
 
