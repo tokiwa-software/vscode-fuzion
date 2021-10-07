ca7e820c234b16e43b7e1faaba6fb5e4649a5d37
Michael Lill <michael.lill@tokiwa.software>
2021-10-07 13:51:26 +0200

    entrypoint.sh change heuristic to determine if container is still in use

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@
 # - shuts down if container is not in use anymore
 #
 
-${OPENVSCODE_SERVER_ROOT}/server.sh &
+${OPENVSCODE_SERVER_ROOT}/server.sh -g Hello.md /home/workspace &
 
 while true
 do
   sleep 60;
-  # if only one socket is in use, container is not used anymore
-  if [ "$(cat /proc/net/sockstat |grep TCP:|cut -d ' ' -f 2,3)" == "inuse 1" ]
+  # if less than two sockets are in use, container is not used anymore
+  if [ "$(cat /proc/net/sockstat |grep TCP:|cut -d ' ' -f 3)" -lt 3 ]
   then
     kill -s 15 -1 && (sleep 10; kill -s 9 -1)
   fi
